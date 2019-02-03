c645144b1d4ff7ba5943a2968f90fe657574889b
chengfei001 <chengfei001@gmail.com>
2019-02-03 17:08:16 +0800

    实现kNN算法

--- kNN/train_test_split.py
+++ kNN/train_test_split.py
@@ -3,3 +3,30 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 import logging
 from sklearn import datasets
+from kNN.model_selection import train_test_split
+
+logging.basicConfig(level=logging.INFO)
+
+iris = datasets.load_iris()
+''' Desc dict_keys(['data', 'target', 'target_names', 'DESCR', 'feature_names', 'filename'])'''
+X = iris.data
+y = iris.target
+
+# shuffle_indexes = np.random.permutation(len(X))
+# logging.info(shuffle_indexes)
+#
+# test_ratio = 0.2
+# test_size = int(len(X) * test_ratio)
+#
+# test_indexes = shuffle_indexes[:test_size]
+# train_indexes = shuffle_indexes[test_size:]
+#
+# X_train = X[train_indexes]
+# y_train = y[train_indexes]
+#
+# X_test = X[test_indexes]
+# y_train = y[train_indexes]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+logging.info(X_train)
