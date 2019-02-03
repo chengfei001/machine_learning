c9971070c8922c54158e6c60920d3c36f7f6c296
chengfei001 <chengfei001@gmail.com>
2019-02-03 20:05:06 +0800

    使用scikit-leran的kNN算法

--- kNN/train_test_split.py
+++ kNN/train_test_split.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import logging
 from sklearn import datasets
 from kNN.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 logging.basicConfig(level=logging.INFO)
 
@@ -29,4 +30,15 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-logging.info(X_train)
+logging.info(X_train.shape)
+logging.info(X_train.shape)
+logging.info("-------------")
+logging.info(X_test.shape)
+logging.info(y_test.shape)
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X_train, y_train)
+y_predict = neigh.predict(X_test)
+logging.info(y_predict)
+logging.info(y_test)
+logging.info('预测准确率：%f', sum(y_predict == y_test) / len(y_test))
