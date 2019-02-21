22ee562adf4e20dbc45cf99a12dfff9675b7c85c
chengfei <chengfei001@gmail.com>
2019-02-21 16:55:53 +0800

    使用dot函数进行矩阵运算提高程序执行效率

--- LinearRegresson/simple_linear_regression.py
+++ LinearRegresson/simple_linear_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import logging
 from LinearRegresson.simple_linear_regssion1 import SimpleLinearRegssion1
+from LinearRegresson.simple_linear_regssion1 import SimpleLinearRegssion2
 
 logging.basicConfig(level=logging.INFO)
 x = np.array([1., 2., 3., 4., 5.])
@@ -35,3 +36,25 @@ reg1.fit(x, y)
 x_hat1 = reg1.predict(np.array([x_prodict]))
 
 logging.info(x_hat1)
+
+'''向量滑算法'''
+m = 100000
+big_x = np.random.random(size=m)
+big_y = big_x + 3.0 + 1.0 + np.random.normal(size=m)
+
+reg2 = SimpleLinearRegssion2()
+
+reg2.fit(big_x, big_y)
+big_y_hot = reg2.predict(np.array(big_x))
+
+logging.info(big_y_hot)
+plt.scatter(big_x, big_y)
+plt.plot(big_x, big_y_hot, color='y')
+plt.show()
+
+logging.info("reg2 finished")
+
+reg11 = SimpleLinearRegssion1()
+
+reg11.fit(big_x, big_y)
+logging.info('reg11.finished')
--- LinearRegresson/simple_linear_regssion1.py
+++ LinearRegresson/simple_linear_regssion1.py
@@ -31,3 +31,32 @@ class SimpleLinearRegssion1:
 
     def _predict(self, x_single):
         return self.a_ * x_single + self.b_
+
+
+class SimpleLinearRegssion2:
+    def __init__(self):
+        """初始化simple linear resgression1 模型"""
+        self.a_ = 0.0
+        self.b_ = 0.0
+
+    def fit(self, x_train, y_train):
+        assert x_train.ndim == 1, '必须为一维数据'
+        assert len(x_train) == len(y_train), 'x_train和y_train的数据量必须相等'
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        d = (x_train - x_mean).dot(x_train - x_mean)
+
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+        return self
+
+    def predict(self, x_predict):
+        assert x_predict.ndim == 1, '数据必须为一维的。'
+        assert self.a_ is not None and self.b_ is not None, '必须是fit后的数据'
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        return self.a_ * x_single + self.b_
